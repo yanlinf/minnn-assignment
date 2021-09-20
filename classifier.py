68910973afb3fb53c8a48245765035905448c32b
Yanlin Feng <fengyanlin@pku.edu.cn>
2021-09-19 23:44:24 -0400

    Change default params

--- classifier.py
+++ classifier.py
@@ -27,10 +27,10 @@ def get_args():
     parser.add_argument("--weight_decay", type=float, default=1e-5)
 
     parser.add_argument("--emb_size", type=int, default=300)
-    parser.add_argument("--hid_size", type=int, default=64)
+    parser.add_argument("--hid_size", type=int, default=100)
     parser.add_argument("--hid_layer", type=int, default=2)
-    parser.add_argument("--word_drop", type=float, default=0.2)
-    parser.add_argument("--emb_drop", type=float, default=0.333)
+    parser.add_argument("--word_drop", type=float, default=0.1)
+    parser.add_argument("--emb_drop", type=float, default=0.4)
     parser.add_argument("--hid_drop", type=float, default=0.5)
     parser.add_argument("--pooling_method", type=str, default="max", choices=["sum", "avg", "max"])
     parser.add_argument("--iters", type=int, default=20)
