2d1dc93958f74d3c91f51020e0c6ea2992192a58
Yanlin Feng <fengyanlin@pku.edu.cn>
2021-09-20 20:51:44 -0400

    Download fasttext on setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from urllib.request import urlretrieve
+import zipfile
+
+download_url = 'https://dl.fbaipublicfiles.com/fasttext/vectors-english/wiki-news-300d-1M.vec.zip'
+donwload_path = 'data/vec.zip'
+
+print('downloading fasttext')
+urlretrieve(download_url, donwload_path)
+
+print('unzipping...')
+with zipfile.ZipFile(donwload_path, 'r') as zip_ref:
+    zip_ref.extractall('data')
+
+print('done')
